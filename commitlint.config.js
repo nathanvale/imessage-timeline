a2e8b873e06dfa6197461edc1714ce741b78ba28
Nathan Vale <hi@nathanvale.com>
2025-11-11 11:49:32 +1100

    chore: configure commitlint to ignore pre-existing invalid commits

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,10 @@
 export default {
   extends: ['@commitlint/config-conventional'],
+  // Ignore commits that pre-date this setup
+  ignores: [
+    (message) => message.includes('[skip ci]'),
+    (message) => message.startsWith('Initial commit'),
+    (message) => message.startsWith('feat: migrate'),
+    (message) => message.startsWith('invalid commit'),
+  ],
 }
